beb4fff6d62087883675178e04b2760703855e83
SujalParajuli-gif <np01cp4a230257@islingtoncollege.edu.np>
2025-12-21 15:06:18 +0545

    added UI elements and shared razor components, UI only for main home page, Streak page and Calender page

--- Components/Data/JournalConstants.cs
+++ Components/Data/JournalConstants.cs
@@ -1,12 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace SecureJournal.Components.Data;
 
-namespace SecureJournal.Components.Data
+public static class JournalConstants
 {
-    internal class JournalConstants
+    public static readonly string[] EntryCategories =
     {
-    }
+        "Work", "Health", "Travel", "Fitness", "Studies", "Family", "Personal Growth", "Reflection", "Other"
+    };
+
+    public static readonly Dictionary<string, string[]> MoodGroups = new()
+    {
+        ["Positive"] = new[] { "Happy", "Excited", "Relaxed", "Grateful", "Confident" },
+        ["Neutral"] = new[] { "Calm", "Thoughtful", "Curious", "Nostalgic", "Bored" },
+        ["Negative"] = new[] { "Sad", "Angry", "Stressed", "Lonely", "Anxious" }
+    };
+
+    public static readonly string[] PrebuiltTags =
+    {
+        "Work","Career","Studies","Family","Friends","Relationships","Health","Fitness","Personal Growth","Self-care",
+        "Hobbies","Travel","Nature","Finance","Spirituality","Birthday","Holiday","Vacation","Celebration","Exercise",
+        "Reading","Writing","Cooking","Meditation","Yoga","Music","Shopping","Parenting","Projects","Planning","Reflection"
+    };
 }
